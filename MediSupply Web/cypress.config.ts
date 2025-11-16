0b74bf06697380f71092c52620eab3bd1fb3ca24
eforero <ingerika.forero@gmail.com>
2025-11-16 14:47:59 -0500

    files

--- MediSupply Web/cypress.config.ts
+++ MediSupply Web/cypress.config.ts
@@ -5,17 +5,12 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://136.112.245.46',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.ts',
     chromeWebSecurity: false,          // ← importante para cross-origin
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 15000,
     pageLoadTimeout: 30000,
     requestTimeout: 15000,
-    responseTimeout: 15000,
-    env: {
-      AUTH_8000: 'http://136.112.245.46:8000',
-      WRONG_AUTH_8001: 'http://136.112.245.46:8001',
-    },
+    responseTimeout: 15000
   },
 })
